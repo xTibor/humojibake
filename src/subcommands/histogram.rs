95630e033c9bd58fef8754dfab9887f996fc56e8
Nagy Tibor <xnagytibor@protonmail.com>
2021-11-08 15:30:30 +0100

    Add sort_by_count flag to histogram

--- src/subcommands/histogram.rs
+++ src/subcommands/histogram.rs
@@ -14,12 +14,15 @@ pub struct HistogramArgs {
 
     #[structopt(help = "Source encoding name. See the `supported-encodings` subcommand for supported encodings.")]
     pub source_encoding: Option<Encoding>,
+
+    #[structopt(long, help = "Sorts results by count")]
+    pub sort_by_count: bool,
 }
 
 impl Subcommand for HistogramArgs {
     fn execute(&self) -> Result<(), Error> {
         let histogram = {
-            let mut result = hexstr_to_vec(&self.input)?
+            let mut results = hexstr_to_vec(&self.input)?
                 .iter()
                 .fold(HashMap::new(), |mut hashmap, &b| {
                     *hashmap.entry(b).or_insert(0) += 1;
@@ -27,8 +30,15 @@ impl Subcommand for HistogramArgs {
                 })
                 .into_iter()
                 .collect::<Vec<(u8, usize)>>();
-            result.sort();
-            result
+
+            if self.sort_by_count {
+                results.sort_by_key(|&(_, count)| count);
+                results.reverse();
+            } else {
+                results.sort_by_key(|&(b, _)| b);
+            }
+
+            results
         };
 
         let max_count_width = histogram.iter().map(|(_, score)| *score).display_width();
